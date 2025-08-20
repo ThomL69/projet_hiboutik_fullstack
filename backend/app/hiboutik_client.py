842812dcbc5cc347c7ca2024dc1319073c2b5cf6
lieghio <thomas.lieghio99@gmail.com>
2025-08-20 17:40:44 +0200

    Adding a new way to store closed_sales without using customers

--- backend/app/hiboutik_client.py
+++ backend/app/hiboutik_client.py
@@ -1,10 +1,9 @@
 import requests
 from app.core.config import settings
 
+# AUTH=(USER, KEY)
 AUTH = (settings.HIBOUTIK_USER, settings.HIBOUTIK_KEY)
 BASE = settings.HIBOUTIK_BASE_URL.rstrip('/')
-
-# AUTH=(USER, KEY)
 
 def get_clients(name: str = None):
     url=f"{BASE}/customers/"
@@ -22,27 +21,68 @@ def get_sales(customer_id: int):
     res.raise_for_status()
     return res.json()
 
+# worked but not optimize
+# def get_closed_sales():
+#     sales_closed = {}
+#     clients = get_clients();
+#     for client in clients:
+#         sales = get_sales(client.get("customers_id"))
+#         for s in sales:
+#             if int(s.get("date_z")) > 0:
+#                 sales_closed[s.get("sale_id")] = s
+#     return sales_closed
 
+
+# Version en essayant une autre URL
+# Recuperation des ventes cloturees
 def get_closed_sales():
     sales_closed = {}
-    clients = get_clients();
-    for client in clients:
-        sales = get_sales(client.get("customers_id"))
-        for s in sales:
-            if s.get("completed_at") != "0000-00-00 00:00:00":
-                sales_closed[s.get("sale_id")] = s
+    sales_id = 1
+    l = []
+    
+    allSales = get_all_sales(sales_id, l)
+    #print(len(allSales))
+
+    for s in allSales:
+        if int(s[0].get("date_z")) > 0:
+            sales_closed[s[0].get("sale_id")] = s
+
     return sales_closed
+    
+            
+# Recuperation d'une vente avec id `sales_id`
+def get_sales_with_id(sales_id: int):
+    url=f"{BASE}/sales/{sales_id}"
+    res = requests.get(url, auth=AUTH)
+    print(f"{sales_id} : {res.status_code}")
+    
+    if(res.status_code != 404):
+        res.raise_for_status()
+        return res.json()
+    else:
+        url=f"{BASE}/sales/{sales_id - 1}"
+        res = requests.get(url, auth=AUTH)
+        res.raise_for_status()
+        return res.json()
+        
 
-# def _get(path: str, params: dict | None = None):
-#     url = f"{BASE}/{path.lstrip('/')}"
-#     res = requests.get(url, auth=AUTH, params=params, timeout=20)
-#     res.raise_for_status()
-#     return res.json()
+# Recuperation de toutes les ventes
+def get_all_sales(sales_id: int, allSales: list):
+    url=f"{BASE}/sales/{sales_id}"
+    res = requests.get(url, auth=AUTH)
+    
+    if(res.status_code != 404):
+        res.raise_for_status()
+        sales = res.json()
+    else:
+        sales = []
 
 
-# --- Resources ---
-# def list_customers() -> list[dict]:
-#     return _get("customers/")
+    if sales == []:
+        allSales.insert(sales_id - 1, sales)
+    else:
+        allSales.insert(sales_id - 1, sales)
+        get_all_sales(sales_id + 1, allSales)
 
-# def list_sales(params: dict | None = None) -> list[dict]:
-#     return _get("sales/", params=params)
+    allSales = list(filter(None, allSales))
+    return allSales
