ab79a80ea554261f12af7ba12fbd136f2dc78e74
lieghio <thomas.lieghio99@gmail.com>
2025-08-22 10:00:28 +0200

    deleting some unused import

--- frontend/src/pages/ClientsPage.js
+++ frontend/src/pages/ClientsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // import Pagination from '../components/Pagination';
 import { useNavigate } from "react-router-dom";
 
